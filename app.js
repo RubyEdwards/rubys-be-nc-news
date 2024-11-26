605d55bcc69eb85249e75283767c7bcd48764146
Ruby Edwards <ruby.vte@gmail.com>
2024-11-26 15:09:53 +0000

    respond to PATCH article path correctly

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const {
   getArticle,
   getArticleComments,
   postComment,
+  patchArticle,
 } = require("./controllers/articles.controller");
 const app = express();
 app.use(express.json());
@@ -22,7 +23,10 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticle);
 app.get("/api/articles/:article_id/comments", getArticleComments);
+
 app.post("/api/articles/:article_id/comments", postComment);
+
+app.patch("/api/articles/:article_id", patchArticle);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "not found" });
