542b9eff734a98c432ef719efb23b87566e9074d
Ruby Edwards <ruby.vte@gmail.com>
2024-11-27 14:35:03 +0000

    update fetchArticle to include comment_count

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,8 +1,27 @@
 const db = require("../db/connection");
 
+// exports.fetchArticle = (id) => {
+//   return db
+//     .query("SELECT * FROM articles WHERE article_id = $1", [id])
+//     .then(({ rows }) => {
+//       if (!rows.length) {
+//         return Promise.reject({ status: 404, msg: "not found" });
+//       }
+//       return rows[0];
+//     });
+// };
+
 exports.fetchArticle = (id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [id])
+    .query(
+      `SELECT articles.author, title, articles.article_id, articles.body, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments)::INT AS comment_count
+  FROM articles
+  LEFT JOIN comments
+  ON articles.article_id = comments.article_id
+  WHERE articles.article_id = $1
+  GROUP BY articles.article_id`,
+      [id]
+    )
     .then(({ rows }) => {
       if (!rows.length) {
         return Promise.reject({ status: 404, msg: "not found" });
