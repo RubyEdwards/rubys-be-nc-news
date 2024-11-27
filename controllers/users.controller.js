11ed61313d63935e8589cde4e18f7bf1cfed0f73
Ruby Edwards <ruby.vte@gmail.com>
2024-11-27 11:12:59 +0000

    remove console logs

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,7 +2,6 @@ const { fetchUsers } = require("../models/users.model");
 
 exports.getUsers = (req, res) => {
   fetchUsers().then((users) => {
-    console.log(users);
     res.status(200).send({ users });
   });
 };
