e4b64764595f3daa1dc864a4e4fa708adf379c14
Ruby Edwards <ruby.vte@gmail.com>
2024-11-28 11:21:34 +0000

    respond correctly to GET user by username endpoint

--- models/users.model.js
+++ models/users.model.js
@@ -5,3 +5,14 @@ exports.fetchUsers = () => {
     return rows;
   });
 };
+
+exports.fetchUser = (username) => {
+  return db
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "not found" });
+      }
+      return rows[0];
+    });
+};
